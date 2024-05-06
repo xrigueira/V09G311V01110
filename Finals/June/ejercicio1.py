380ec2259b1fa9a69aed316626e69304a366fc70
Xurxo Rigueira <xrigueiradiaz@gmail.com>
2024-05-06 18:04:58 +0200

    Final v1

--- Finals/June/ejercicio1.py
+++ Finals/June/ejercicio1.py
@@ -5,7 +5,7 @@ Un menú con la siguientes opciones
 
     1. Crear un fichero
     2. Meter datos en el fichero
-    3. Leer del dichero y sumar
+    3. Leer de dichero y sumar
     4. Fin
 
 En la opción 1 se creará el fichero con el nombre y dirección que indique el usuario y, además,
@@ -30,13 +30,101 @@ En cualquier otra opción se debe visualizar un mensaje de error.
 Nota importante: se evaluará que las funciones reciban parámetros y devuelvan algún tipo de dato.
 """
 
-import os.path
+import os
 
+# Una función que transforma un fichero de texto en una matriz.
+def text2matrix(filepath):
+    matrix = []
+    with open(filepath, 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            matrix.append(list(line))
+    return matrix
+
+# Una función que transforma una matriz en un fichero de texto.
+def matrix2text(matrix, filepath):
+    with open(filepath, 'w') as file:
+        for row in matrix:
+            file.write(','.join(row) + '\n')
+
+# Definir función para crear el fichero en caso de que no exista
 def crear_fichero():
 
-    # Acceder al directorio actual
+    # Preguntar al usuario cuál es el fichero sobre el que se va a ejecutar el programa
+    ruta = os.getcwd() + '\\Finals\\June'
+    print('La ruta actual es:', ruta)
+
+    nombre_fichero = input('Introduzca el nombre del fichero: ')
+    ruta_fichero = ruta + '\\' + nombre_fichero + '.txt'
+    print('La ruta del fichero es:', ruta_fichero)
+
+    # Notificar al usuario si ya existe un fichero con el nombre indicado
+    if os.path.exists(ruta_fichero):
+        print('Ya existe un fichero con el nombre indicado')
+
+        # En caso de que exista un fichero con el nombre indicado, imprimir la información que contiene el fichero
+        with open(ruta_fichero, 'r') as fichero:
+            for line in fichero:
+                print(line)
+    else:
+        fichero = open(ruta_fichero, 'w')
+        fichero.close()
+    
+    return ruta_fichero
+
+# Definir función para la carga de datos
+def carga_datos(ruta_fichero):
+    
+    # opcion_carga = input("Queres meter datos no ficheiro (S/n)?: ")
+    opcion_carga = "S"
+    
+    datos = text2matrix(ruta_fichero)
+
+    while "N" != opcion_carga.upper():
+        codigo_producto = str(input("Dame el nombre del mineral: "))
+        nombre_producto = str(input("Dame el peso atómico: ")) 
+        precio = str(input("Dame el precio/kg: "))
+        cantidad_almacen = str(input("Dame el total: "))
+        datos.append([codigo_producto, nombre_producto, precio, cantidad_almacen])
+        
+        opcion_carga = input("¿Quieres meter más datos (n=no)? ")
+
+    matrix2text(datos, ruta_fichero)
+
+# Función de modificar datos
+def modificar_fichero(ruta_fichero):
+    
+    # Leer los datos
+    datos = text2matrix(ruta_fichero)
+    
+    cambio = str(input("¿Qué quieres cambiar el nombre del mineral (m) o el peso atómico (c) o nada(n)? " ))
+    
+    # Extraer los productos
+    productos = []
+    for producto in datos:
+        productos.append(producto[1])
+    
+    if cambio.lower() == "p":
+        producto = str(input("Dame el nombre del producto: "))
+        if producto in productos:
+            precio = str(input("Dame el precio: "))
+            datos[productos.index(producto)][2] = precio
+    
+    elif cambio.lower() == "c":
+        producto = str(input("Dame el nombre del producto: "))
+        if producto in productos:
+            cantidad = str(input("Dame la cantidad: "))
+            datos[productos.index(producto)][3] = cantidad
+    
+    elif cambio.lower() == "n":
+        print ("Sin cambios ")
+    
+    matrix2text(datos, ruta_fichero)
+
+def generar_informe(ruta_fichero):
     pass
 
+# Menú de opciones
 opcion = 0
 while opcion != 4:
 
@@ -54,14 +142,14 @@ while opcion != 4:
         
         # Definir la opcion 1
         if opcion == 1:
-            pass
+            crear_fichero()
         
         # Definir la opcion 2
         elif opcion == 2:
-            pass
+            modificar_fichero()
 
         elif opcion == 3:
-            pass
+            generar_informe()
 
     # En caso de que no se introduzca un numero
     except ValueError:
--- Finals/June/ejercicio_continua.py
+++ Finals/June/ejercicio_continua.py
@@ -1,115 +1,160 @@
+"""
+Definir un programa en Python que permita cargar, modificar y listar datos de productos en un archivo.
+El programa debe tener un menú con las siguientes opciones:
+
+    1. Cargar nuevos datos en el archivo: 
+    El usuario podrá ingresar el código, nombre, precio y cantidad en almacén de un producto y estos 
+    datos se guardarán en el archivo.
+
+    2. Modificar datos existentes en el archivo: 
+    El usuario podrá seleccionar un producto del archivo y elegir si desea cambiar el precio o 
+    la cantidad en almacén. Luego, podrá ingresar el nuevo valor y este se actualizará en el archivo.
+
+    3. Listar todos los elementos del archivo: 
+    El programa mostrará en pantalla todos los productos 
+    almacenados en el archivo, mostrando su código, nombre, precio y cantidad.
+
+    4. Salir del programa: 
+    El programa finalizará su ejecución.
+
+El programa debe permitir al usuario ingresar el nombre del archivo con el que desea trabajar. 
+Si el archivo no existe, se creará automáticamente.
+"""
+
 import os
 
-#Función de carga de datos
-def cargadatos(nomefich):
-    #abro o ficheiro para engadir datos
-    ficheiro = open(nomefich,'a')
-    #creo os datos que vou utilizar
-    CodProd = ""
-    NomProd = ""
-    Precio = 0.0
-    CantAlm = 0
-    opc = input("Queres meter datos no ficheiro? ")
-    while "N" != opc.upper():
-        CodProd = str(input("Dame el código del producto: "))
-        NomProd = str(input("Dame el nombre del producto: ")) 
-        Precio = float(input("Dame el precio: "))
-        CantAlm= int(input("Dame la cantidad en almacén: "))
-        ficheiro.write(CodProd + "\r") #grabo os datos
-        ficheiro.write(NomProd + "\r")
-        ficheiro.write(str(Precio)+"\r")
-        ficheiro.write(str(CantAlm) + "\r")
-        opc = input("¿Quieres meter mas datos (N=no)? ")        
-    ficheiro.close()
+# Una función que transforma un fichero de texto en una matriz.
+def text2matrix(filepath):
+    matrix = []
+    with open(filepath, 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            matrix.append(list(line))
+    return matrix
 
-#Función de listado
-def listado(nomefich):
-    os.system("cls")
-    print ("LISTADO")
-    #abro o ficheiro para ler datos
-    ficheiro = open(nomefich,'r')
-    print ("Código"+"\t"+"Nombre"+ "\t"*2+ "Precio"+"\t"+"Cantidad")
-    for lineaCod in ficheiro: #utilizo de pivote para ler e de control do ciclo o dato do nome
-        lineaNom = ficheiro.readline()
-        lineaPrecio = ficheiro.readline()
-        lineaCant = ficheiro.readline()
-        print(lineaCod[:-1]+"\t"+lineaNom[:-1]+"\t"*2+lineaPrecio[:-1]+"\t"+lineaCant) #o -1 e para quitar o salto de renglón
-    #unha vez acabado pecho o ficheiro
-    ficheiro.close()
+# Una función que transforma una matriz en un fichero de texto.
+def matrix2text(matrix, filepath):
+    with open(filepath, 'w') as file:
+        for row in matrix:
+            file.write(','.join(row) + '\n')
 
-#Función de modificar datos
-def modifica(nomefich):
-    os.system("cls")
-    #abro o ficheiro para ler datos e outro para copiar
-    ficheiro1 = open(nomefich,'r')
-    ficheiro2 = open("copia.txt", 'w')
-    for lineaCod in ficheiro1:
-        lineaNom = ficheiro1.readline()
-        lineaPrecio = ficheiro1.readline()
-        lineaCant = ficheiro1.readline()
-        os.system("cls")
-        print("Código = {0} Nome = {1}".format(lineaCod[:-1],lineaNom[:-1]))
-        print("Precio = {0} Cantidad = {1}".format(lineaPrecio[:-1],lineaCant[:-1]))
-        cambio = str(input("¿Qué quieres cambiar el precio(p) o la cantidad en almacén (c) o nada(n)? " ))
-        if cambio.lower() == "p":
-            precio = float(input("Dame el Precio: "))
-            if precio < 0.0 :
-                Precioleido = float(lineaPrecio[:-1])
-            else:
-                Precioleido = precio
-            lineaPrecio = str(Precioleido)+"\r"
-        elif cambio.lower() == "c":
-            cantidad = int(input("Dame la cantidad: "))
-            if cantidad <= 0.0 :
-                Cantidadleida = int(lineaCant[:-1])
-            else:
-                Cantidadleida = int(lineaCant[:-1]) + cantidad
-            lineaCant = str(Cantidadleida)+"\r"
-        elif cambio.lower() == "n":
-            print ("Sin cambios ")            
-        print(" ")
-        ficheiro2.write(lineaCod)
-        ficheiro2.write(lineaNom)
-        ficheiro2.write(lineaPrecio)
-        ficheiro2.write(lineaCant)
-    #unha vez acabado pecho os ficheiros
-    ficheiro1.close()
-    ficheiro2.close()
-    #fago a copia do ficheiro copia.txt no ficheiro principal
-    os.remove(nomefich)
-    os.rename('copia.txt',nomefich)
+# Definir función para crear el fichero en caso de que no exista
+def crear_fichero():
 
-#Creo, se non existe, o ficheiro co que se vai traballar
-def creafich():
-    nomefich = str(input("Dime o nome do ficheiro coa ruta "))
-    existe = os.path.isfile(nomefich)
-    if existe == False:
-        ficheiro = open(nomefich,'w') #si no existe lo creo
-        ficheiro.close() #pechoo sempre porque abrirase dependendo da opción
-    return nomefich
+    # Preguntar al usuario cuál es el fichero sobre el que se va a ejecutar el programa
+    ruta = os.getcwd() + '\\Finals\\June'
+    print('La ruta actual es:', ruta)
 
-#menu principal
+    nombre_fichero = input('Introduzca el nombre del fichero: ')
+    ruta_fichero = ruta + '\\' + nombre_fichero + '.txt'
+    print('La ruta del fichero es:', ruta_fichero)
+
+    # Notificar al usuario si ya existe un fichero con el nombre indicado
+    if os.path.exists(ruta_fichero):
+        print('Ya existe un fichero con el nombre indicado')
+
+        # En caso de que exista un fichero con el nombre indicado, imprimir la información que contiene el fichero
+        with open(ruta_fichero, 'r') as fichero:
+            for line in fichero:
+                print(line)
+    else:
+        fichero = open(ruta_fichero, 'w')
+        fichero.close()
+    
+    return ruta_fichero
+
+# Definir función para la carga de datos
+def carga_datos(ruta_fichero):
+    
+    # opcion_carga = input("Queres meter datos no ficheiro (S/n)?: ")
+    opcion_carga = "S"
+    
+    datos = text2matrix(ruta_fichero)
+
+    while "N" != opcion_carga.upper():
+        codigo_producto = str(input("Dame el código del producto: "))
+        nombre_producto = str(input("Dame el nombre del producto: ")) 
+        precio = str(input("Dame el precio: "))
+        cantidad_almacen = str(input("Dame la cantidad en almacén: "))
+        datos.append([codigo_producto, nombre_producto, precio, cantidad_almacen])
+        
+        opcion_carga = input("¿Quieres meter más datos (n=no)? ")
+
+    matrix2text(datos, ruta_fichero)
+
+# Función de modificar datos
+def modificar_fichero(ruta_fichero):
+    
+    # Leer los datos
+    datos = text2matrix(ruta_fichero)
+    
+    cambio = str(input("¿Qué quieres cambiar el precio (p) o la cantidad en almacén (c) o nada(n)? " ))
+    
+    # Extraer los productos
+    productos = []
+    for producto in datos:
+        productos.append(producto[1])
+    
+    if cambio.lower() == "p":
+        producto = str(input("Dame el nombre del producto: "))
+        if producto in productos:
+            precio = str(input("Dame el precio: "))
+            datos[productos.index(producto)][2] = precio
+    
+    elif cambio.lower() == "c":
+        producto = str(input("Dame el nombre del producto: "))
+        if producto in productos:
+            cantidad = str(input("Dame la cantidad: "))
+            datos[productos.index(producto)][3] = cantidad
+    
+    elif cambio.lower() == "n":
+        print ("Sin cambios ")
+    
+    matrix2text(datos, ruta_fichero)
+
+# Función de listado
+def listado(nombre_fichero):
+    
+    # Abrir el fichero para leer datos
+    fichero = open(nombre_fichero, 'r')
+    for line in fichero:
+        elementos = line.split(',')
+        print ("Código: ", elementos[0], "Nombre: ", elementos[1], "Precio: ", elementos[2], "Cantidad: ", elementos[3])
+    
+    # Cerrar el fichero
+    fichero.close()
+
+# Menu principal
 def menu():
-    ficheiro = ""
-    ficheiro = creafich()
+    
+    fichero = crear_fichero()
     opcion = ""
     while opcion.upper() != "F":
-        os.system("cls")
-        print("Menú")
-        print("\tC)Cargar nuevos datos en el fichero")
-        print("\tM)Modificar alguno de los datos del fichero (solo precio y cantidad)")
-        print("\tL)Listar todos los elementos dle fichero")
-        print("\tF)FIN")
-        opcion = input("\nOpción: ")
-        if opcion.upper() == "C":
-            cargadatos(ficheiro)
-        elif opcion.upper() == "M":
-            modifica(ficheiro)
-        elif opcion.upper() == "L":
-            listado(ficheiro)
-        elif opcion.upper() == "F":
-            print ("Fin de programa")
-        else : print("opción non válida")
+        try:
+            # os.system("cls")
+            opcion = input("\nMenú:\n   C)Cargar nuevos datos en el fichero\n   M)Modificar alguno de los datos del fichero (solo precio y cantidad)\n   L)Listar todos los elementos dle fichero\n   F)FIN\n   Teclear la opción a continuación: ")
+            
+            if opcion.upper() not in ["C", "M", "L", "F"]:
+                print('Opción no válida')
 
-#Programa principal
+            if opcion.upper() in ["C", "M", "L", "F"]:
+                print('Ha elegido la opción:', opcion)
+
+            if opcion.upper() == "C":
+                carga_datos(fichero)
+            
+            elif opcion.upper() == "M":
+                modificar_fichero(fichero)
+            
+            elif opcion.upper() == "L":
+                listado(fichero)
+            
+            elif opcion.upper() == "F":
+                print ("Fin de programa")
+        
+        
+        except ValueError:
+            print('Por favor, introduzca una cadena')
+
+# Programa principal
 menu()
